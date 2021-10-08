132541db79985a694ec8c259866ebc320b8d85c9
Samyak-Bumb <vijaybumb0575@gmail.com>
2021-10-08 18:33:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,10 +5,12 @@ input.addEventListener("keyup", function(event){
    document.getElementById("search-btn").click();
   }
 });
+
 const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 const result = document.getElementById("result");
 const sound = document.getElementById("sound");
 const btn = document.getElementById("search-btn");
+
 btn.addEventListener("click",()=>{
     let inpWord = document.getElementById("inp-word").value;
     fetch(`${url}${inpWord}`)
@@ -32,12 +34,15 @@ btn.addEventListener("click",()=>{
                 <p class="word-example">
                     ${data[0].meanings[0].definitions[0].example || ""}
                 </p>`;
+
             sound.setAttribute("src", `https:${data[0].phonetics[0].audio}`);
         })
+
         .catch(()=>{
             result.innerHTML = `<h3 class="error">Couldn't Find The Word</h3>`;
     });
 });
+
 function playSound(){
     sound.play();
 }
